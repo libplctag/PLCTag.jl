edf2711fcd565b8e4e74e7a61d46a78dd153b8a2
Keith Rutkowski <keith@analytech-solutions.com>
2019-09-23 15:19:07 -0400

    Add PLCTag abstraction layer over the C library

--- bin/example.jl
+++ bin/example.jl
@@ -1,55 +1,17 @@
 using PLCTag
 
 
-const TAG_PATH     = "protocol=ab-eip&gateway=127.0.0.1&path=1,5&cpu=micro800&elem_size=4&elem_count=200&name=TestBigArray&debug=4"
-const ELEM_COUNT   = 200
-const ELEM_SIZE    = 4
-const DATA_TIMEOUT = 5000
-
-
 function main()
-	tag = PLCTag.C.plc_tag_create(TAG_PATH, DATA_TIMEOUT)
-	tag <= 0 && error("ERROR $(PLCTag.C.plc_tag_decode_error(tag)): Could not create tag!")
+	plc = PLC(
+		cpu = "compactlogix",
+		protocol = "ab-eip",
+		gateway = "192.168.1.1",
+		path = "1,0",
+	)
 	
-	try
-		# code = PLCTag.C.plc_tag_lock(tag)
-		# code == PLCTag.C.PLCTAG_STATUS_OK || error("ERROR: Unable to obtain tag lock! Got error code $(code): $(unsafe_string(PLCTag.C.plc_tag_decode_error(code)))")
-		
-		code = PLCTag.C.plc_tag_status(tag)
-		code == PLCTag.C.PLCTAG_STATUS_OK || error("ERROR: Error setting up tag internal state. Got error code $(code): $(unsafe_string(PLCTag.C.plc_tag_decode_error(code)))")
-		
-		# read values
-		code = PLCTag.C.plc_tag_read(tag, DATA_TIMEOUT)
-		code == PLCTag.C.PLCTAG_STATUS_OK || error("ERROR: Unable to read the data! Got error code $(code): $(unsafe_string(PLCTag.C.plc_tag_decode_error(code)))")
-		
-		for i in 1:ELEM_COUNT
-			val = PLCTag.C.plc_tag_get_int32(tag, (i-1)*ELEM_SIZE)
-			@info "data[$(i)] = $(val)"
-		end
-		
-		# set new values
-		for i in 1:ELEM_COUNT
-			val = PLCTag.C.plc_tag_get_int32(tag, (i-1)*ELEM_SIZE)
-			val = val+1
-			PLCTag.C.plc_tag_set_int32(tag, (i-1)*ELEM_SIZE, val)
-		end
-		
-		code = PLCTag.C.plc_tag_write(tag, DATA_TIMEOUT)
-		code == PLCTag.C.PLCTAG_STATUS_OK || error("ERROR: Unable to write the data! Got error code $(code): $(unsafe_string(PLCTag.C.plc_tag_decode_error(code)))")
-		
-		# read values again
-		code = PLCTag.C.plc_tag_read(tag, DATA_TIMEOUT)
-		code == PLCTag.C.PLCTAG_STATUS_OK || error("ERROR: Unable to read the data! Got error code $(code): $(unsafe_string(PLCTag.C.plc_tag_decode_error(code)))")
-		
-		for i in 1:ELEM_COUNT
-			val = PLCTag.C.plc_tag_get_int32(tag, (i-1)*ELEM_SIZE)
-			@info "data[$(i)] = $(val)"
-		end
-		
-		# PLCTag.C.plc_tag_unlock(tag)
-	finally
-		PLCTag.C.plc_tag_destroy(tag)
-	end
+	str = String(plc, "str")
+	@info str
 end
 
 main()
+
--- src/PLCTag.jl
+++ src/PLCTag.jl
@@ -1,7 +1,10 @@
 module PLCTag
 	include("c.jl")
 	
-	export PLC, PLCRef, PLCBinding
+	export PLC, PLCRef
+	
+	
+	const DATA_TIMEOUT = 1000
 	
 	
 	Base.@kwdef struct PLC
@@ -9,7 +12,7 @@ module PLCTag
 		protocol::String
 		gateway::String
 		path::String
-		debug::Int = 5  # TODO: for production, this should probably be 0 or 1
+		debug::Int = 1
 	end
 	
 	
@@ -22,11 +25,11 @@ module PLCTag
 			
 			path = plcpath(plc, tagName, sizeof(T), count)
 			
-			tag = PLCTag.C.plc_tag_create(path, 1000)  # 1000 is create time-out
-			tag <= 0 && error("Unable to create tag `$(tagName)` of type `$(T)` and count $(count): $(unsafe_string(PLCTag.C.plc_tag_decode_error(tag)))")
+			tag = C.plc_tag_create(path, DATA_TIMEOUT)
+			tag <= 0 && error("Unable to create tag `$(tagName)` of type `$(T)` and count $(count)")
 			
 			result = new{T}(Ref{Int32}(tag), count)
-			finalizer(t -> PLCTag.C.plc_tag_destroy(t[]), result.tag)
+			finalizer(t -> C.plc_tag_destroy(t[]), result.tag)
 			return result
 		end
 	end
@@ -37,34 +40,25 @@ module PLCTag
 	Base.eltype(ref::PLCRef{T}) where {T} = T
 	Base.iterate(ref::PLCRef, state = 1) = state > length(ref) ? nothing : (ref[state], state+1)
 	
-	function Base.read(ref::PLCRef)
-		code = PLCTag.C.plc_tag_read(ref.tag[], 0)
-		code in (PLCTag.C.PLCTAG_STATUS_OK, PLCTag.C.PLCTAG_STATUS_PENDING) || error("Unable to read tag: $(unsafe_string(PLCTag.C.plc_tag_decode_error(code)))")
-	end
-	function Base.fetch(ref::PLCRef, ind::Int = 1)
-		timedwait(() -> PLCTag.C.plc_tag_status(ref.tag[]) == PLCTag.C.PLCTAG_STATUS_OK, 1.0, pollint = 0.01) === :ok || error("Failed to complete tag read: $(unsafe_string(PLCTag.C.plc_tag_decode_error(PLCTag.C.plc_tag_status(ref.tag[]))))")
-		return convert(eltype(ref), plcget(eltype(ref), ref.tag[], ind))
-	end
-	
-	function Base.write(ref::PLCRef, val, ind::Int = 1)
-		plcset(eltype(ref), ref.tag[], ind, convert(eltype(ref), val))
-		code = PLCTag.C.plc_tag_write(ref.tag[], 0)
-		code in (PLCTag.C.PLCTAG_STATUS_OK, PLCTag.C.PLCTAG_STATUS_PENDING) || error("Unable to write tag: $(unsafe_string(PLCTag.C.plc_tag_decode_error(code)))")
-	end
-	function Base.flush(ref::PLCRef)
-		timedwait(() -> PLCTag.C.plc_tag_status(ref.tag[]) == PLCTag.C.PLCTAG_STATUS_OK, 1.0, pollint = 0.01) === :ok || error("Failed to complete tag write: $(unsafe_string(PLCTag.C.plc_tag_decode_error(PLCTag.C.plc_tag_status(ref.tag[]))))")
-	end
-	
 	function Base.getindex(ref::PLCRef, ind::Int = 1)
-		read(ref)
-		return fetch(ref, ind)
+		1 <= ind <= length(ref) || error("Index $(ind) is out of range for PLCRef of count $(ref.count)")
+		
+		code = C.plc_tag_read(ref.tag[], DATA_TIMEOUT)
+		code == C.PLCTAG_STATUS_OK || error("ERROR: Unable to read the data! Got error code $(code): $(unsafe_string(C.plc_tag_decode_error(code)))")
+		
+		val = plcget(eltype(ref), ref.tag[], ind-1)
+		return convert(eltype(ref), val)
 	end
 	
 	function Base.setindex!(ref::PLCRef, val, ind::Int = 1)
-		write(ref, val, ind)
-		flush(ref)
+		1 <= ind <= length(ref) || error("Index $(ind) is out of range for PLCRef of count $(ref.count)")
+		
+		code = plcset(eltype(ref), ref.tag[], ind-1, convert(eltype(ref), val))
+		code == C.PLCTAG_STATUS_OK || error("ERROR: Unable to write the data! Got error code $(code): $(unsafe_string(C.plc_tag_decode_error(code)))")
+		
+		code = C.plc_tag_write(ref.tag[], DATA_TIMEOUT)
+		code == C.PLCTAG_STATUS_OK || error("ERROR: Unable to write the data! Got error code $(code): $(unsafe_string(C.plc_tag_decode_error(code)))")
 	end
-	
 	
 	
 	plcpath(plc::PLC, tagName::String, size::Int, count::Int) = join(map(((k, v),) -> "$(k)=$(v)", (
@@ -78,40 +72,26 @@ module PLCTag
 		:name => tagName,
 	)), '&')
 	
-	
-	const PLCPrimitives = Union{
-		Bool,
-		Int8, UInt8, Int16, UInt16,
-		Int32, UInt32, Int64, UInt64,
-		Float32, Float64
-	}
-	
+	# TODO: create Bool versions as well
 	for T in (
-		:Bool,
 		:Int8, :UInt8, :Int16, :UInt16,
 		:Int32, :UInt32, :Int64, :UInt64,
 		:Float32, :Float64,
 	)
-		if T === :Bool
-			# NOTE: using Int8 (aka SINT on the PLC) to store a Bool
-			@eval plcget(::Type{Bool}, tag::Int32, ind::Int)            = plcget(Int8, tag, ind) != 0
-			@eval plcset(::Type{Bool}, tag::Int32, ind::Int, val::Bool) = plcset(Int8, tag, ind, convert(Int8, val))
-		else
-			@eval plcget(::Type{$(T)}, tag::Int32, ind::Int)            = PLCTag.C.$(Symbol("plc_tag_get_", lowercase(String(T))))(tag, ind-1)
-			@eval plcset(::Type{$(T)}, tag::Int32, ind::Int, val::$(T)) = PLCTag.C.$(Symbol("plc_tag_set_", lowercase(String(T))))(tag, ind-1, val)
-		end
+		@eval plcget(::Type{$(T)}, args...; kwargs...) = C.$(Symbol("plc_tag_get_", lowercase(String(T))))(args...; kwargs...)
+		@eval plcset(::Type{$(T)}, args...; kwargs...) = C.$(Symbol("plc_tag_set_", lowercase(String(T))))(args...; kwargs...)
 		
 		@eval Base.$(T)(plc::PLC, tagName::String) = PLCRef{$(T)}(plc, tagName)[]
+		@eval function Base.Vector{$(T)}(plc::PLC, tagName::String, count::Int)
+			ref = PLCRef{$(T)}(plc, tagName, count)
+			return $(T)[ref[i] for i in 1:length(ref)]
+		end
 	end
 	
-	function Base.Vector{T}(plc::PLC, tagName::String, count::Int) where {T<:PLCPrimitives}
-		ref = PLCRef{T}(plc, tagName, count)
-		return T[ref[i] for i in 1:length(ref)]
-	end
 	
 	function Base.String(plc::PLC, tagName::String)
-		len = PLCRef{Int32}(plc, "$(tagName).LEN")[]
-		data = PLCRef{UInt8}(plc, "$(tagName).DATA", len)
+		len = PLCRef{Int32}(plc, "$(tagName).LEN")
+		data = PLCRef{UInt8}(plc, "$(tagName).DATA", len[])
 		return String(data)
 	end
 end
